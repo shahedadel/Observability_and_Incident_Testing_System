86f7094a57faf8bf1522f5fbacfbd355afd4f749
Shahed Alabdulrahman <shaheda@umich.edu>
2026-05-18 21:04:40 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Deployment Demo App Running 🚀");
+  throw new Error("Intentional runtime crash for incident simulation");
 });
 
 app.get("/health", (req, res) => {
